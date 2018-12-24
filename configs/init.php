46378bd2d54b574b8ea9b64eb734d8e17bb55621
Ushelets <Goldberg7777@gmail.com>
2018-12-25 00:28:11 +0400

    + twig, но без функции

--- configs/init.php
+++ configs/init.php
@@ -3,7 +3,18 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/templates');
 
-$twig = new Twig_Environment($loader, array(
+$twig = new Twig_Environment($loader, [
  'cache' => 'compilation_cache',
  'auto_reload' => true,
-));
+]);
+
+/* $twig->addFunction('MoneyRus', new Twig_Function('MoneyRus'));
+function MoneyRus($money)
+{
+    $fmt = new NumberFormatter('ru_RU', NumberFormatter::DECIMAL);
+    $a = $fmt->format($money);
+    if (intl_is_failure($fmt->getErrorCode())) {
+    report_error("Formatter error");
+    }
+    return $a;
+} */
